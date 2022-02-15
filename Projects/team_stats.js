0c3a9e6d353078d082b2084f297e69346e0b1566
oksanagylka <oksana.gylka.job@gmail.com>
2022-02-14 19:27:53 -0500

    added some code

--- Projects/team_stats.js
+++ Projects/team_stats.js
@@ -1,5 +1,36 @@
+const team = {
+    _players: [
+        {
+            firstName: 'Pablo',
+            lastName: 'Sanchez',
+            age: 11
+        }
+    ],
+    _games: [
+        {
+            opponent: 'Broncos',
+            teamPoints: 42,
+            opponentPoints: 27
+        }],
+    get players() {
 
+    },
+    get games() {
 
+    },
+    addPlayer(firstName, lastName, age) {
+        let player = {
+            firstName: firstName,
+            lastName: lastName,
+            age: age
+        };
+        this._players.push(player);
+    },
+};
+team.addPlayer('Steph', 'Curry', 28);
+team.addPlayer('Lisa', 'Leslie', 44);
+team.addPlayer('Bugs', 'Bunny', 76);
+console.log(team._players);
 
 
 
